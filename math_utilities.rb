4a837b36ac9e4d4fa272e1fb571c70525a804d7b
Alberto De Lucca <albertodelucca@gmail.com>
2014-09-26 09:34:25 -0430

    Fix perfect square predicate

--- math_utilities.rb
+++ math_utilities.rb
@@ -7,9 +7,11 @@ class Numeric
     end
   end
   def perfect_square?()
+    last_digit = self.to_s.split("").last.to_i
+    return false if last_digit == 2 || last_digit == 3 || last_digit == 7 || last_digit == 8 
     n = self.digital_root
-    if n == 0 || n == 1 || n == 4 || n == 7 
-      n % 4 == 0 && n % 4 == 1 && n % 4 == 2 ? true : false
+    if n == 1 || n == 4 || n == 7 || n == 9 
+      self % 4 <= 2
     else
       return false
     end
