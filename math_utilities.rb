030f0bd7eaefdd3e9073625ff1698f74d5dc3071
Alberto De Lucca <albertodelucca@gmail.com>
2014-09-26 06:03:00 -0430

    Add perfect square predicate

--- math_utilities.rb
+++ math_utilities.rb
@@ -6,4 +6,12 @@ class Numeric
       self.to_s.split("").map {|n| n.to_i}.reduce(:+).digital_root
     end
   end
+  def perfect_square?()
+    n = self.digital_root
+    if n == 0 || n == 1 || n == 4 || n == 7 
+      n % 4 == 0 && n % 4 == 1 && n % 4 == 2 ? true : false
+    else
+      return false
+    end
+  end
 end
