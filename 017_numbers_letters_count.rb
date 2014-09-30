9808e2bf395297b57b2dd12fed6314939344cf2c
Alberto De Lucca <albertodelucca@gmail.com>
2014-09-30 16:17:37 -0430

    Add method to return strings to numbers < 10 != 0
    This method will be used to construct bigger number strings
    afterwards.

--- 017_numbers_letters_count.rb
+++ 017_numbers_letters_count.rb
@@ -1,10 +1,23 @@
 class Numeric
-  def in_strings
-    self
+  def in_words
+    final_string = []
+  end
+  def units
+    case self
+    when 1 then "One"
+    when 2 then "Two"
+    when 3 then "Three"
+    when 4 then "Four"
+    when 5 then "Five"
+    when 6 then "Six"
+    when 7 then "Seven"
+    when 8 then "Eight"
+    when 9 then "Nine"
+    end
   end
 end
 
 ary = []
-1.upto(1000) { |num| ary << num.in_strings }
+1.upto(1000) { |num| ary << num.in_words }
 string = ary.join("")
 print string.gsub("-", "").gsub(" ","").length
